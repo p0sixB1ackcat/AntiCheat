f65f1a5c628168caf6795ed96bc79ee2eacf15a9
p0sixB1ackcat <p0sixb1ackcat@gmail.com>
2019-12-08 18:24:55 +0800

    Base Code...

---
+++ Anti-Cheat/Anti-CheatCallback.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Anti-CheatBase.h"
+
+#define PROCESS_VM_WRITE       (0x0020)
+#define PROCESS_VM_READ        (0x0010)
+#define PROCESS_VM_OPERATION   (0x0008)
+
+EXTERN_C_START
+
+OB_PREOP_CALLBACK_STATUS
+AntiCheatOBPreOperationCallback (
+    _In_ PVOID RegistrationContext,
+    _Inout_ POB_PRE_OPERATION_INFORMATION OperationInformation
+    );
+
+VOID
+AnticheatObPostOperationCallback (
+    _In_ PVOID RegistrationContext,
+    _In_ POB_POST_OPERATION_INFORMATION OperationInformation
+    );
+
+VOID
+AnticheatLoadImageRoutine(
+    _In_opt_ PUNICODE_STRING FullImageName,
+    _In_ HANDLE ProcessId,
+    _In_ PIMAGE_INFO ImageInfo
+);
+
+FLT_PREOP_CALLBACK_STATUS
+AnticheatFltAcquireSectionSyncPreRoutine(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+);
+
+FLT_POSTOP_CALLBACK_STATUS
+AntiCheatFltAcquireSectionSyncPostRoutine(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags
+);
+
+
+
+
+
+EXTERN_C_END
---
+++ Anti-Cheat/runsdvui.cmd
@@ -0,0 +1,2 @@
+cd /d "F:\Code\Cheat\Anti-Cheat\Anti-Cheat" &msbuild "Anti-Cheat.vcxproj" /t:sdvViewer /p:configuration="Debug" /p:platform="x64" /p:SolutionDir="F:\Code\Cheat\Anti-Cheat" 
+exit %errorlevel% 
