b2630a9c197d9a248e518ea0ae6f1452ca131782
p0sixB1ackcat <p0sixb1ackcat@gmail.com>
2019-12-11 23:27:32 +0800

    Clear...

--- Anti-Cheat/runsdvui.cmd
+++
@@ -1,2 +0,0 @@
-cd /d "F:\Code\Cheat\Anti-Cheat\Anti-Cheat" &msbuild "Anti-Cheat.vcxproj" /t:sdvViewer /p:configuration="Debug" /p:platform="x64" /p:SolutionDir="F:\Code\Cheat\Anti-Cheat" 
-exit %errorlevel% 
